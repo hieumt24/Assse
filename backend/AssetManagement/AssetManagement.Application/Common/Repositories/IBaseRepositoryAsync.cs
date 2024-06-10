f39c5ae7cfb5faeb674500f6b242798635d098b8
Hiếu Mai Trọng <hieumthe163841@fpt.edu.vn>
2024-06-10 21:23:58 +0700

    initialize backend and frontend project

---
+++ backend/AssetManagement/AssetManagement.Application/Common/Repositories/IBaseRepositoryAsync.cs
@@ -0,0 +1,29 @@
+﻿using AssetManagement.Domain.Common.Models;
+using AssetManagement.Domain.Common.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Application.Common.Repositories
+{
+    public interface IBaseRepositoryAsync<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(Guid id);
+
+        Task<List<T>> ListAllAsync();
+
+        Task<IList<T>> ListAsync(ISpecification<T> spec);
+
+        Task<T> FirstOrDefaultAsync(ISpecification<T> spec);
+
+        Task<T> AddAsync(T entity);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task<T> DeleteAsync(Guid id);
+
+        Task<int> CountAsync(ISpecification<T> spec);
+    }
+}
