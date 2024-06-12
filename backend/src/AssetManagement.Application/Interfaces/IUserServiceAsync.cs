5e9f0b4e024c247f81b97b5fce423b65a0ac4b5b
Tran Dung <99379498+Underline007@users.noreply.github.com>
2024-06-12 21:56:35 +0700

    feat: view list user and detail user (#23)
    Co-authored-by: Mai Trong Hieu <90890367+hieumt24@users.noreply.github.com>

--- backend/src/AssetManagement.Application/Interfaces/IUserServiceAsync.cs
+++ backend/src/AssetManagement.Application/Interfaces/IUserServiceAsync.cs
@@ -1,11 +1,15 @@
 ﻿using AssetManagement.Application.Models.DTOs.Users.Requests;
 using AssetManagement.Application.Models.DTOs.Users;
 using AssetManagement.Application.Wrappers;
+using AssetManagement.Application.Models.DTOs.Users.Responses;
+using AssetManagement.Domain.Enums;
 
 namespace AssetManagement.Application.Interfaces
 {
     public interface IUserServiceAsync
     {
         Task<Response<UserDto>> AddUserAsync(AddUserRequestDto request);
+        Task<Response<List<UserResponseDto>>> GetAllUsersAsync(string? search, string? orderBy, bool isDescending, int skip, int take, EnumLocation? adminLocation);
+        Task<Response<UserResponseDto>> GetUserByIdAsync(Guid userId);
     }
 }
