2c855e266cd718619ad04e666a8e1642fc21a9b5
Vu Thanh Long <98462074+have2b@users.noreply.github.com>
2024-06-13 10:47:37 +0700

    feat: context wrapper for app (#33)

--- frontend/src/services/auth/authService.ts
+++ frontend/src/services/auth/authService.ts
@@ -1,10 +1,11 @@
 import axiosInstance from "@/api/axiosInstance";
-import { LoginReq } from "@/models";
+import { FirstTimeLoginReq, LoginReq } from "@/models";
 
 export const loginService = (req: LoginReq) => {
   return axiosInstance
     .post("/auth", req)
     .then((res) => {
+      localStorage.setItem("token", res.data.token);
       return {
         success: true,
         message: "Login successfully!",
@@ -13,5 +14,24 @@ export const loginService = (req: LoginReq) => {
     })
     .catch((err) => {
       return { success: false, message: "Failed to login.", error: err };
+    });
+};
+
+export const firstTimeService = (req: FirstTimeLoginReq) => {
+  return axiosInstance
+    .post("/auth/first-time", req.newPassword)
+    .then((res) => {
+      return {
+        success: true,
+        message: "Change password successfully!",
+        data: res.data,
+      };
+    })
+    .catch((err) => {
+      return {
+        success: false,
+        message: "Failed to change password.",
+        error: err,
+      };
     });
 };
--- frontend/src/validations/authSchema.ts
+++ frontend/src/validations/authSchema.ts
@@ -21,3 +21,17 @@ export const loginSchema = z.object({
     .max(50, { message: "Username must be less than 50 characters" }),
   password: passwordSchema,
 });
+
+export const firstTimeLoginSchema = z.object({
+  newPassword: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(50, { message: "Password must be less than 50 characters" })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: "Password must contain at least one special character",
+    }),
+});
