d65623e6e378c07866e859d57f135bc09274ff53
Mai Trong Hieu <90890367+hieumt24@users.noreply.github.com>
2024-06-14 10:58:56 +0700

    fix : pagaination user list (#57)
    * feat: login user
    
    * fix : role id in entites
    
    * feat: add change password user
    
    * fix: add role claims at jwt token
    
    * fix: update pagination in list user
    
    * fix :update pagaination list user

--- backend/src/AssetManagement.Application/Interfaces/IUserServiceAsync.cs
+++ backend/src/AssetManagement.Application/Interfaces/IUserServiceAsync.cs
@@ -1,9 +1,9 @@
-﻿using AssetManagement.Application.Models.DTOs.Users.Requests;
-using AssetManagement.Application.Models.DTOs.Users;
-using AssetManagement.Application.Wrappers;
-using AssetManagement.Application.Models.DTOs.Users.Responses;
-using AssetManagement.Domain.Enums;
 using AssetManagement.Application.Filter;
+using AssetManagement.Application.Models.DTOs.Users;
+using AssetManagement.Application.Models.DTOs.Users.Requests;
+using AssetManagement.Application.Models.DTOs.Users.Responses;
+using AssetManagement.Application.Wrappers;
+using AssetManagement.Domain.Enums;
 
 namespace AssetManagement.Application.Interfaces
 {
@@ -11,7 +11,7 @@ namespace AssetManagement.Application.Interfaces
     {
         Task<Response<UserDto>> AddUserAsync(AddUserRequestDto request);
 
-        Task<PagedResponse<List<UserResponseDto>>> GetAllUsersAsync(PaginationFilter validFilter, string? search, string? orderBy, bool isDescending, EnumLocation? adminLocation, string route);
+        Task<PagedResponse<List<UserResponseDto>>> GetAllUsersAsync(PaginationFilter filter, string? search, string? orderBy, bool isDescending, EnumLocation? adminLocation, string route);
 
         Task<Response<UserResponseDto>> GetUserByIdAsync(Guid userId);
 
