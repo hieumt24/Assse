d79705205169c58a601cf2cdfecef5384ed86f6c
dT0210 <121219177+dT0210@users.noreply.github.com>
2024-06-20 09:10:03 +0700

    fix: change password msg

--- frontend/src/services/auth/authService.ts
+++ frontend/src/services/auth/authService.ts
@@ -34,7 +34,7 @@ export const firstTimeService = (req: FirstTimeLoginReq) => {
     .catch((err) => {
       return {
         success: false,
-        message: "Current password is incorrect.",
+        message: err.response.data.message,
         data: err,
       };
     });
