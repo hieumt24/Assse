6b69b71eaa3a6060e20f4870c82537c8b3f7b95b
tlongdepzaivaicalon <thanhlongvu156@gmail.com>
2024-06-24 11:02:04 +0700

    fix: created and edited asset now show on top of list at the first time

--- frontend/src/components/forms/asset/CreateAssetForm.tsx
+++ frontend/src/components/forms/asset/CreateAssetForm.tsx
@@ -119,7 +119,7 @@ export const CreateAssetForm: React.FC = () => {
       });
       if (res.success) {
         toast.success("Asset created successfully!");
-
+        localStorage.setItem("orderBy", "createdon");
         navigate("/assets");
       } else {
         toast.error(res.message);
