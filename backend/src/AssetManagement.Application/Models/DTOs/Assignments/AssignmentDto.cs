8d8a4b3c0f53de0284794752a5bd4c91931867bf
Hiếu Mai Trọng <hieumthe163841@fpt.edu.vn>
2024-06-25 15:28:07 +0700

    fix : base entity

--- backend/src/AssetManagement.Application/Models/DTOs/Assignments/AssignmentDto.cs
+++ backend/src/AssetManagement.Application/Models/DTOs/Assignments/AssignmentDto.cs
@@ -1,10 +1,8 @@
 ﻿using AssetManagement.Domain.Common.Models;
-using AssetManagement.Domain.Entites;
 using AssetManagement.Domain.Enums;
 
 namespace AssetManagement.Application.Models.DTOs.Assignments
 {
-    public class AssignmentResponseDto
     public class AssignmentDto : BaseEntity
     {
         public Guid Id { get; set; }
@@ -14,7 +12,6 @@ namespace AssetManagement.Application.Models.DTOs.Assignments
         public string? AssignedBy { get; set; }
         public DateTime AssignedDate { get; set; }
         public EnumLocation Location { get; set; }
-        public EnumAssignmentStatus Status { get; set; }
         public EnumAssignmentStatus Status { get; set; }
         public string Note { get; set; } = string.Empty;
     }
