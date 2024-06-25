c431557b1be81736012d153c8c5a1fb08d2516f8
Vu Thanh Long <98462074+have2b@users.noreply.github.com>
2024-06-25 09:26:25 +0700

    ui: dialog for reset password (#154)

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -10,14 +10,6 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import { ADMIN_NAV_FUNCTIONS, BREADCRUMB_COMPONENTS } from "@/constants";
 import { useAuth } from "@/hooks";
 import useClickOutside from "@/hooks/useClickOutside";
@@ -25,6 +17,7 @@ import { useCallback, useRef, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { ChangePasswordForm } from "./forms/user/ChangePasswordForm";
+import { GenericDialog } from "./shared";
 import { Separator } from "./ui/separator";
 
 export const Header = () => {
@@ -108,32 +101,15 @@ export const Header = () => {
             open={openChangePassword}
             onOpenChange={setOpenChangePassword}
           />
-          <Dialog open={openLogout} onOpenChange={setOpenLogout}>
-            <DialogTrigger className="w-full py-2 text-start text-sm transition-all hover:bg-zinc-200">
-              <p className="ms-4 font-medium">Log out</p>
-            </DialogTrigger>
-            <DialogContent className="border-2">
-              <DialogHeader>
-                <DialogTitle className="text-center text-2xl font-bold text-red-600">
-                  Are you sure?
-                </DialogTitle>
-                <DialogDescription className="text-center text-lg">
-                  Do you want to logout?
-                </DialogDescription>
-                <div className="flex items-center justify-center gap-4">
-                  <Button variant={"destructive"} onClick={handleLogout}>
-                    Log out
-                  </Button>
-                  <Button
-                    variant="outline"
-                    onClick={() => setOpenLogout(false)}
-                  >
-                    Cancel
-                  </Button>
-                </div>
-              </DialogHeader>
-            </DialogContent>
-          </Dialog>
+          <GenericDialog
+            trigger="Log out"
+            title="Are you sure?"
+            desc="Do you want to logout?"
+            confirmText="Log out"
+            onConfirm={handleLogout}
+            open={openLogout}
+            setOpen={setOpenLogout}
+          />
         </CollapsibleContent>
       </Collapsible>
     </div>
