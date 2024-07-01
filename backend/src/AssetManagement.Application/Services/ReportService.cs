133c7ed397c3fb08195ea61ff2472b7dd7e5f651
Mai Trong Hieu <90890367+hieumt24@users.noreply.github.com>
2024-07-01 14:53:22 +0700

    Feat/manage request (#220)
    * fix : fix disable user
    feat : add disable return request
    
    * fix : create username
    
    * feat: get assignment by user
    
    * fix : update filter order by asset code in view list asset
    
    * feat : update time token
    
    * fix : fix return request
    
    * fix : disable user when user have assignment

---
+++ backend/src/AssetManagement.Application/Services/ReportService.cs
@@ -0,0 +1,15 @@
+﻿using AssetManagement.Application.Filter;
+using AssetManagement.Application.Interfaces.Services;
+using AssetManagement.Application.Models.DTOs.Reports.Responses;
+using AssetManagement.Application.Wrappers;
+
+namespace AssetManagement.Application.Services
+{
+    public class ReportService : IReportServices
+    {
+        public Task<PagedResponse<ReportResponseDto>> FilterReportAsync(PaginationFilter pagination, string? orderBy, bool? isDescending, string? route)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
