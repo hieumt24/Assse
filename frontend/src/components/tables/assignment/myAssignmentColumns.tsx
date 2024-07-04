cf4f040c6944bf1ace4c226945be0d243cbdbbbc
Underline007 <99379498+Underline007@users.noreply.github.com>
2024-07-04 21:15:09 +0700

    fix: do not show returned state in my asm

--- frontend/src/components/tables/assignment/myAssignmentColumns.tsx
+++ frontend/src/components/tables/assignment/myAssignmentColumns.tsx
@@ -71,8 +71,7 @@ export const myAssignmentColumns = ({
           return <p className="text-red-600">Declined</p>;
         case 4:
           return <p className="text-yellow-600">Waiting for returning</p>;
-        case 5:
-          return <p className="text-blue-600">Returned</p>;
+
         default:
           return <p>Not found state</p>;
       }
