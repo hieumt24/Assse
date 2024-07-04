2f23785f4c66f8d54684a9066d8eb4b5e2e6d51d
Mai Trong Hieu <90890367+hieumt24@users.noreply.github.com>
2024-07-04 10:32:55 +0700

    Feat/manage request (#252)
    * fix : fix disable user
    feat : add disable return request
    
    * fix : create username
    
    * feat: get assignment by user
    
    * fix : update filter order by asset code in view list asset
    
    * feat : update time token
    
    * fix : fix return request
    
    * fix : disable user when user have assignment
    
    * feat : new feature report
    
    * fix : sort by role in view user list
    
    * fix : update sort assignment
    
    * fix : order by priorty
    
    * fix : change sort in many view list
    
    * fix : sort by in view user, ass, return request
    
    * fix : config merge main
    
    * fix : sort by list
    
    * fix : sort by user, asset, assignemnt list
    
    * feat : custom middleware exception authentication
    
    * feat : pagaination report
    
    * feat : update state last modifier all entity

--- backend/src/AssetManagement.Application/Models/DTOs/Reports/Responses/ReportResponseDto.cs
+++ backend/src/AssetManagement.Application/Models/DTOs/Reports/Responses/ReportResponseDto.cs
@@ -8,7 +8,7 @@
         public int Assigned { get; set; }
         public int Available { get; set; }
         public int NotAvailable { get; set; }
-        public int Waitingforrecycling { get; set; }
+        public int WaitingForRecycling { get; set; }
         public int Recycled { get; set; }
     }
 }
