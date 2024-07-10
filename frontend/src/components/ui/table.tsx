9962399767b689e68b347aea8da55ecac93b48f0
dT0210 <duthanh41025@gmail.com>
2024-07-11 06:35:52 +0700

    fix: minor ui changes

--- frontend/src/components/ui/table.tsx
+++ frontend/src/components/ui/table.tsx
@@ -90,7 +90,7 @@ const TableCell = React.forwardRef<
     ref={ref}
     className={cn(
       "p-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
-      "overflow-hidden text-ellipsis whitespace-nowrap",
+      "overflow-hidden text-ellipsis",
       className,
     )}
     {...props}
